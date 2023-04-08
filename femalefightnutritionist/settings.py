f4a8b6a5ffdce6360020dcb21ac7c4adc1d9e86b
Sam <sam.mcnally94@gmail.com>
2023-04-08 17:13:08 +0100

    add: tailwind

--- femalefightnutritionist/settings.py
+++ femalefightnutritionist/settings.py
@@ -45,7 +45,7 @@ LOCAL = env('LOCAL')
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = ['femalefightnutritionist.herokuapp.com']
+ALLOWED_HOSTS = ['femalefightnutritionist.herokuapp.com', '127.0.0.1']
 
 INTERNAL_IPS = [
     "127.0.0.1",
