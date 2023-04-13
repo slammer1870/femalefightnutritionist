51a151ced0a44b047b80b961d231ce24079a959f
Sam <sam.mcnally94@gmail.com>
2023-04-13 13:17:10 +0100

    remove: stripe secret key

--- femalefightnutritionist/settings.py
+++ femalefightnutritionist/settings.py
@@ -47,8 +47,6 @@ LOCAL = env('LOCAL')
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
-STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
-
 ALLOWED_HOSTS = ['femalefightnutritionist.herokuapp.com', '127.0.0.1']
 
 INTERNAL_IPS = [
