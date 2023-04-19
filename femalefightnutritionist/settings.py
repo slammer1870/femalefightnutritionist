1ff5553c47320393e3a6c9b9fd1ec195354a0404
Sam <sam.mcnally94@gmail.com>
2023-04-19 21:43:35 +0100

    chenage: whitenoise

--- femalefightnutritionist/settings.py
+++ femalefightnutritionist/settings.py
@@ -185,7 +185,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
