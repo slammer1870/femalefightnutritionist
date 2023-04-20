a5f00d63fab806ba3a810cdff3d7cfca41236493
Sam <sam.mcnally94@gmail.com>
2023-04-20 08:49:36 +0100

    update: whitenoise

--- femalefightnutritionist/settings.py
+++ femalefightnutritionist/settings.py
@@ -186,11 +186,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
