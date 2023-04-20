e978c7c4af8e6f1b27ebd15e4b20ef43e2ea0136
Sam <sam.mcnally94@gmail.com>
2023-04-20 08:46:37 +0100

    add: staticfiles

--- femalefightnutritionist/settings.py
+++ femalefightnutritionist/settings.py
@@ -178,7 +178,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -187,9 +187,8 @@ STATICFILES_DIRS = (
 )
 
 STORAGES = {
-    # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.ManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
