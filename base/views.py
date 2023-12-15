829c8131885e14b57a6d6482b731a06a35cea874
Sam <sam.mcnally94@gmail.com>
2023-12-15 16:07:47 +0000

    update: blog to published

--- base/views.py
+++ base/views.py
@@ -24,7 +24,7 @@ class IndexPageView(generic.FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['blog_posts'] = Post.objects.all()[:3]
+        context['blog_posts'] = Post.objects.filter(published=True)[:3]
         context['contact_form'] = ContactForm
 
         return context
